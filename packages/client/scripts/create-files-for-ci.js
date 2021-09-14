0d7f28f84424f3d7aab85f40c4e7aa8b52ef217c
Kazumune Masaki <kazumune.masaki@bitcoinbank.co.jp>
2021-09-14 11:29:45 +0900

    fix create-files-for-ci script

--- packages/client/scripts/create-files-for-ci.js
+++ packages/client/scripts/create-files-for-ci.js
@@ -1,9 +1,16 @@
 const fs = require("fs")
 
 const CFG_FILE = "./src/config-firebase/firebaseConfig.ts"
-const body = process.env.FIREBASE_CONFIG
-
-if (body !== undefined && !fs.existsSync(CFG_FILE)) {
-  fs.writeFileSync(CFG_FILE, body)
-  console.log(`Created ${CFG_FILE}`)
+if (fs.existsSync(CFG_FILE)) {
+  return
 }
+console.log("Config file missing")
+
+const body = process.env.FIREBASE_CONFIG
+if (body === undefined) {
+  return
+}
+console.log(`Config file in env var: ${body}`)
+
+fs.writeFileSync(CFG_FILE, body)
+console.log(`Created ${CFG_FILE}`)
