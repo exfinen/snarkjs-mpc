269af269b519e0820a4d8c0ee2399205c5d5bb7e
Kazumune Masaki <kazumune.masaki@bitcoinbank.co.jp>
2021-09-14 11:11:30 +0900

    create firebase config with script when missing

--- packages/client/scripts/create-files-for-ci.js
+++ packages/client/scripts/create-files-for-ci.js
@@ -1,8 +1,9 @@
 const fs = require("fs")
 
 const CFG_FILE = "./src/config-firebase/firebaseConfig.ts"
-const body = process.ENV.FIREBASE_CONFIG
+const body = process.env.FIREBASE_CONFIG
 
 if (body !== undefined && !fs.existsSync(CFG_FILE)) {
   fs.writeFileSync(CONFIG_FILE, body)
+  console.log(`Created ${CFG_FILE}`)
 }
