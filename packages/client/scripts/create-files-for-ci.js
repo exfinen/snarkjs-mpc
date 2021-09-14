4a9648ef3c72dffde2bdae8648c038cef508fe81
Kazumune Masaki <kazumune.masaki@bitcoinbank.co.jp>
2021-09-14 11:23:15 +0900

    fix create-files-for-ci script

--- packages/client/scripts/create-files-for-ci.js
+++ packages/client/scripts/create-files-for-ci.js
@@ -4,6 +4,6 @@ const CFG_FILE = "./src/config-firebase/firebaseConfig.ts"
 const body = process.env.FIREBASE_CONFIG
 
 if (body !== undefined && !fs.existsSync(CFG_FILE)) {
-  fs.writeFileSync(CONFIG_FILE, body)
+  fs.writeFileSync(CFG_FILE, body)
   console.log(`Created ${CFG_FILE}`)
 }
